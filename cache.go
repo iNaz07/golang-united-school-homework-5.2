8bf4e6dd1c1ef3b97954cb75b2880eb619434a8c
iNaz07 <yfqnbfqajy5S>
2022-05-20 16:27:11 +0600

    implemented

--- cache.go
+++ cache.go
@@ -3,21 +3,54 @@ package cache
 import "time"
 
 type Cache struct {
+	CacheMap map[string]Value
+}
+
+type Value struct {
+	Val   string
+	ExpAt *time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	cache := make(map[string]Value)
+	return Cache{
+		CacheMap: cache,
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	value, ok := c.CacheMap[key]
+	if !ok {
+		return "", false
+	}
+	if value.ExpAt.Before(time.Now()) {
+		delete(c.CacheMap, key)
+		return "", false
+	}
+	return value.Val, true
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	val := Value{Val: value}
+	c.CacheMap[key] = val
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	keys := []string{}
+	for k, v := range c.CacheMap {
+		if !v.ExpAt.Before(time.Now()) {
+			keys = append(keys, k)
+		} else {
+			delete(c.CacheMap, k)
+		}
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	val := Value{
+		Val:   value,
+		ExpAt: &deadline,
+	}
+	c.CacheMap[key] = val
 }
